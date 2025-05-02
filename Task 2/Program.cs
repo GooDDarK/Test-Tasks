207d412cf85fe08e1dfae27f29c2341ccbc4a2f5
GooDDarK <petrasan1998@yandex.ru>
2025-05-02 12:28:16 +0300

    Сделал 2 задачу

---
+++ Task 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Используйте команду GetCount, чтобы прочитать переменную \"count\". Ее можно использовать параллельно.\nИспользуйте команду AddToCount \"цифра\", чтобы добавить значение к переменной \"count\". Ее нельзя использовать одновременно!");
+
+            string userText = Console.ReadLine();
+
+            if (userText == "GetCount")
+            {
+                int serverCount = Server.GetCount();
+
+                Console.WriteLine("count = " + serverCount);
+            }
+
+            if (userText.Contains("AddToCount"))
+            {
+                string stringValue = userText.Replace("AddToCount", "").Trim();
+
+                if (stringValue == "")
+                {
+                    Console.WriteLine("Укажите на сколько нужно увеличить значение \"count\"");
+
+                    stringValue = Console.ReadLine();
+
+                    if (stringValue == "")
+                        return;
+                }
+
+                int addValue = int.Parse(stringValue);
+
+                Server.AddToCount(addValue);
+
+                Console.WriteLine("Переменная \"count\" увеличена на " + addValue);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task 2/Server.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_2
+{
+    public static class Server
+    {
+        static int count = 0;
+        static ReaderWriterLockSlim rw = new ReaderWriterLockSlim();
+
+        public static int GetCount()
+        {
+            rw.EnterReadLock();
+
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                rw.ExitReadLock();
+            }
+        }
+
+        public static void AddToCount(int addValue)
+        {
+            rw.EnterWriteLock();
+
+            try
+            {
+                count += addValue;
+            }
+            finally
+            {
+                rw.ExitWriteLock();
+            }
+        }
+    }
+}
